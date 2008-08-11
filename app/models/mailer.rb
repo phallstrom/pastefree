fe929a04dfaef3f285efba105f7bb8c0294ea76e
Philip Hallstrom <philip@pjkh.com>
2008-08-11 16:09:52 -0700

    make the confirmation email have a nicer from/subject

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,9 +1,9 @@
 class Mailer < ActionMailer::Base
   
   def user_confirmation(user)
-    subject    'Confirm Your Email Address'
+    subject    'PasteFree - Confirm Your Email Address'
     recipients user.email
-    from       ''
+    from       'Paste Free <pastefree@pjkh.com>'
     sent_on    Time.now
     
     body       :token => user.generate_token
