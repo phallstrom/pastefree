60d6ffbef2d4ca4fa119d38edf0815a8ff3ee993
Philip Hallstrom <philip@pjkh.com>
2008-08-13 21:54:20 -0700

    signin/signout capability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,28 @@ class HomeController < ApplicationController
   #
   #
   #
+  def signout
+    cookies[:token] = { :value => nil, :path => '/', :expires => 1.year.ago } 
+    redirect_to root_path
+  end
+
+  #
+  #
+  #
+  def signin
+    @user = User.find_or_create_by_email(params[:email])
+    if @user.valid?
+      ActionMailer::Base.default_url_options[:host] = request.host_with_port # an evil necessity
+      Mailer.deliver_user_confirmation(@user)
+      render :nothing => true, :status => 200
+    else
+      render :nothing => true, :status => 400
+    end
+  end
+
+  #
+  #
+  #
   def confirm_user
     @user = User.find_by_token(params[:token])
 
@@ -21,6 +43,9 @@ class HomeController < ApplicationController
     redirect_to mine_pastes_path
   end
 
+  #
+  #
+  #
   def time_to_upgrade
     render :layout => false
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.statistics '/statistics', :controller => 'home', :action => 'statistics'
   map.about '/about', :controller => 'home', :action => 'about'
+  map.signout '/signout', :controller => 'home', :action => 'signout'
+  map.signin '/signin', :controller => 'home', :action => 'signin'
   map.time_to_upgrade '/time-to-upgrade-your-browser', :controller => 'home', :action => 'time_to_upgrade'
   map.confirm_user '/confirm/:token', :controller => 'home', :action => 'confirm_user'
 
