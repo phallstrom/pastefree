dd8502e1a8815b8933b70da8a07e79fafe229ae7
Philip Hallstrom <philip@pjkh.com>
2008-09-24 19:35:13 -0700

    merge statistics into about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => 'pastes', :action => 'new'
 
-  map.statistics '/statistics', :controller => 'home', :action => 'statistics'
   map.about '/about', :controller => 'home', :action => 'about'
   map.time_to_upgrade '/time-to-upgrade-your-browser', :controller => 'home', :action => 'time_to_upgrade'
 
